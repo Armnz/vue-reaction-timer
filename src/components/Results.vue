ed3d868ed93cef7c597790b1a89d89052659dbda
Armand <armands.elsbergs@gmail.com>
2024-01-14 23:52:38 +0200

    initial commit

---
+++ src/components/Results.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <p>Reaction time: {{ score }} ms.</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+};
+</script>
+
+<style scoped></style>
